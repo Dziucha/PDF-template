24ba323cdc269d9adb152e54093915626a2fbe04
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-15 12:53:16 +0100

    multipage pdf creator - qty of pages per topic specified in csv with topics

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fpdf import FPDF
+import pandas
+
+topics = pandas.read_csv("topics-2.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-pdf.set_font(family="Times", size=12)
-pdf.cell(w=0, h=12, txt="Hello there!", align="L", ln=1)
-pdf.set_font(family="Times", style="B", size=10)
-pdf.cell(w=0, h=12, txt="Hi there!", align="L", ln=1)
+for index, row in topics.iterrows():
+    number_of_pages = int(row["Pages"])
+
+    for i in range(number_of_pages):
+        pdf.add_page()
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=0, h=12, txt=f"{row['Topic']} page {i+1}", align="L", ln=1)
+
 pdf.output("output.pdf")
